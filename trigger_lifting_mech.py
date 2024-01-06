8196a343a6df457212d3d23d7125c0aa1896b94e
sanket369 <skhade369@gmail.com>
2024-01-06 16:04:38 +0530

    test trigger

---
+++ trigger_lifting_mech.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32
+from move_base_msgs.msg import MoveBaseActionResult
+
+def move_base_result_callback(data):
+    if data.status.status == 3 and data.status.text == "Goal reached.":
+        # Publish the message on the /lifting_mech topic
+        lifting_mech_pub.publish(1)
+
+if __name__ == '__main__':
+    rospy.init_node('lifting_mech_publisher')
+
+    # Create a publisher for the /lifting_mech topic
+    lifting_mech_pub = rospy.Publisher('/lifting_mech', Int32, queue_size=10)
+
+    # Subscribe to the /move_base/result topic
+    move_base_result_sub = rospy.Subscriber('/move_base/result', MoveBaseActionResult, move_base_result_callback)
+
+    rospy.spin()
